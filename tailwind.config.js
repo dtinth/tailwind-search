62aea54a8970228c80f530bdbff248ab1d0a52d4
Thai Pangsakulyanont <dtinth@spacet.me>
2020-04-05 03:58:43 +0700

    Initial version

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: [
+        'Arimo',
+        'Helvetica',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      mono: [
+        'Cousine',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
+    },
+    extend: {
+      colors: {
+        e9e8e7: '#e9e8e7',
+        '8b8685': '#8b8685',
+        '656463': '#656463',
+        '454443': '#454443',
+        '353433': '#353433',
+        '252423': '#252423',
+        '090807': '#090807',
+        bbeeff: '#bbeeff',
+        d7fc70: '#d7fc70',
+      },
+    },
+  },
+  variants: {},
+  plugins: [require('@tailwindcss/custom-forms')],
+}
